5d575cc18ec79425a60e6d03ccbbc3a10dbfcf2a
root <root@c49352a7d8ea.(none)>
2015-11-26 20:00:33 +0000

    ^ for test script

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./run_cache.sh &
 ./run_client.sh &
-bin/text_guided_planner cfg/test.cfg run=t1 > output/t1/learner_all.log
+bin/text_guided_planner cfg/test.cfg run=t1 2>&1 > output/t1/learner_all.log
