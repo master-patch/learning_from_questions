e6ff321391c3ef26811e2d67d123ca2bcad4e93a
nicola <notsecurity@gmail.com>
2015-11-28 14:49:35 -0500

    making sentences a map instead of a list to keep the ids

--- code/ir/baseline.py
+++ code/ir/baseline.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     sSentenceFile = '../../data/minecraft_text.raw'
     lSentences = ReadSentencesFromTextFileSimple(sSentenceFile)
     sentences = [sentence.lWords for sentence in lSentences]
+    ids = [sentence.iIndex for sentence in lSentences]
 
-    bags = BagOfWords(sentences)
-    print bags.question("action", "wood")
+    bags = BagOfWords(sentences, ids)
+    print bags.question("object", "bed")[0]
