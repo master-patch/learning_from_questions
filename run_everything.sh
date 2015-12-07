41d73d9f2253ce9cf05494750b94b4608e5cf177
Nicola Greco <me@nicolagreco.com>
2015-12-06 21:28:51 -0500

    testing changes

--- run_everything.sh
+++ run_everything.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 ./run_cache.sh &
 ./run_client.sh &
+./run_ir.sh &
+sleep 3 &&
 ./run_learner.sh
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t questionbot .
-docker run -d questionbot sh ./run_everything.sh -v tmp:/tmp
+docker run -ti questionbot sh ./run_everything.sh -v tmp:/tmp
