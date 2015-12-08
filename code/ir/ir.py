ee0937f64c3b1788c3ef827778dc0f65318953c5
nicola <notsecurity@gmail.com>
2015-12-07 20:32:56 -0500

    QA now doesn't answer only 34

--- code/ir/ir.py
+++ code/ir/ir.py
@@ -1,15 +1,26 @@
+from nltk.stem.porter import PorterStemmer
 from collections import defaultdict
 from vocab import P, O, A
-import sys
 import random
+import sys
 
 # Random is not really random anymore
 random.seed(1)
+stemmer = PorterStemmer()
 
 
 # Build the inverted index {word: [sent_id1, sentid_2]}
-def build_inverted_index(vocab, sentences, k=0, shuffle=False):
-    index = defaultdict(list)
+# wordpair = ("make-wood", ['make wood'])
+def build_inverted_index(vocab_annotated, sentences, k=0, shuffle=False):
+    vocab = set()
+    for pddl, alternatives in vocab_annotated.iteritems():
+        for alternative in alternatives:
+            for word in alternative.split(' '):
+                word = unicode(word, 'utf-8')
+                word = stemmer.stem(word)
+                vocab.add(word)
+
+    index = defaultdict(set)
     sent_ids = sentences.keys()
 
     if shuffle:
@@ -18,10 +29,28 @@ def build_inverted_index(vocab, sentences, k=0, shuffle=False):
     for sent_id in sent_ids:
         sentence = sentences[sent_id]
         for word in sentence:
+            word = unicode(word, 'utf-8')
+            word = stemmer.stem(word)
             if word in vocab:
                 if k == 0 or len(index[word]) < k:
-                    index[word].append(sent_id)
+                    index[word].add(sent_id)
+
+    print index
     return index
+
+
+def search_inverted_index(vocab_annotated, index, pddl):
+    alternatives = vocab_annotated[pddl]
+    sentences = set()
+    for alternative in alternatives:
+        intersection = set.intersection(
+            *[index[stemmer.stem(w)] for w in alternative.split(' ')]
+        )
+
+        for sent_id in intersection:
+            sentences.add(sent_id)
+
+    return sentences
 
 
 # Abstract implementation of the IR system
@@ -69,14 +98,15 @@ class BagOfWords(AbstractIR):
             sentences,
             ids)
 
-        self.vocab = P | O | A
+        vocab_all = O.copy()
+        vocab_all.update(A)
+        self.vocab = vocab_all
+
         self.index = build_inverted_index(
             self.vocab,
             self.sentences,
             k,
             shuffle)
-
-        # # print ids
 
         # build up a cache
         self.cache = dict()
@@ -95,37 +125,38 @@ class BagOfWords(AbstractIR):
                 sent_id = int(split[len(split) - 1])
                 if sent_id not in self.cache:
                     self.cache[sent_id] = []
-                    # # print sent_id, l
                 self.cache[sent_id].append(l)
-            # # print sorted(self.cache.keys()), len(self.cache.keys())
 
     # ask question to the IR engine
+    def search_index(self, pddl):
+        return search_inverted_index(self.vocab, self.index, pddl)
+
     def question(self, type, question):
         parsed = self.parse_question(question)
 
         if type == "action":
             a = parsed
-            print "IR: 'Tell me more about how to {}'".format(
-                a.replace('-', ' '))
+            # print "IR: 'Tell me more about how to {}'".format(
+            #     a.replace('-', ' '))
 
-            indexes = self.index[a]
+            indexes = self.search_index(a)
         elif type == "object":
             o = parsed
-            print "IR: 'Tell me more about the object {}'".format(o)
-            indexes = self.index[o]
+            # print "IR: 'Tell me more about the object {}'".format(o)
+            indexes = self.search_index(o)
         elif type == "subgoal":
             p, o = parsed
             indexes = set.intersect(
-                self.index[p],
-                self.index[o])
+                self.search_index(p),
+                self.search_index(o))
         elif type == "subgoal_pair":
             p1, o1, p2, o2 = parsed
-            p_matching = set.intersect(
-                self.index[p1],
-                self.index[p2])
-            o_matching = set.intersect(
-                self.index[o1],
-                self.index[o2])
+            p_matching = set.intersection(
+                self.search_index(p1),
+                self.search_index(p2))
+            o_matching = set.intersection(
+                self.search_index(o1),
+                self.search_index(o2))
             indexes = p_matching | o_matching
         else:
             indexes = []
